6b8605fd08e728c97246984ea342d423e12ed727
justSerjke <serjke3@gmail.com>
2022-05-09 20:24:27 +0400

    First Try

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -44,15 +44,5 @@ public class UserDaoImp implements UserDao {
         }
         return car != null ? car.getUser() : null;
     }
-
-
-//      try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-//         String HQL="FROM Address addr LEFT OUTER JOIN FETCH addr.employee WHERE addr.addressId=:addrId";
-//         Address address = session.createQuery(HQL, Address.class).setParameter("addrId", 1).uniqueResult();
-//         System.out.println(address);
-//         System.out.println(address.getEmployee());
-//      } catch (HibernateException e) {
-//         e.printStackTrace();
-//      }
 }
 
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,75 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "series")
+    private int series;
+
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car)) return false;
+
+        Car car = (Car) o;
+
+        if (getSeries() != car.getSeries()) return false;
+        return getModel().equals(car.getModel());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getModel().hashCode();
+        result = 31 * result + getSeries();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
